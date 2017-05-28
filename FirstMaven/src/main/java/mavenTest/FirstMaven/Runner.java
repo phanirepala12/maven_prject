51a5b1205a4f4389850ea9b617ddcf1f39988619
Phani Repala <phanirepala12>
2017-05-27 20:03:07 -0400

    First maven project

---
+++ FirstMaven/src/main/java/mavenTest/FirstMaven/Runner.java
@@ -0,0 +1,12 @@
+package mavenTest.FirstMaven;
+
+import java.util.concurrent.Callable;
+
+public class Runner implements Callable<Integer>{
+
+	public Integer call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
